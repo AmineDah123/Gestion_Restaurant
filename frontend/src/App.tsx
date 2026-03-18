8c3ab8a83784ad9a2a735cbb8a9620d8819a71b4
AmineDah123 <aminedahmane044@gmail.com>
2026-03-18 01:05:04 +0000

    new project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
